3965a78d4eea67df175b079ed9510a190c92d2af
willa <wuyidan0012@163.com>
2023-07-11 18:06:25 +0800

    first attempt

---
+++ MDK/Obj/stm32f10x_rcc.d
@@ -0,0 +1,10 @@
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\src\stm32f10x_rcc.c
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\inc\stm32f10x_rcc.h
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\M3\stm32f10x.h
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\M3\core_cm3.h
+.\obj\stm32f10x_rcc.o: D:\RuanJianXiaZai\KEIL\ARM\ARMCC\Bin\..\include\stdint.h
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\M3\core_cmInstr.h
+.\obj\stm32f10x_rcc.o: D:\RuanJianXiaZai\KEIL\pack\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_armcc.h
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\M3\core_cmFunc.h
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\M3\system_stm32f10x.h
+.\obj\stm32f10x_rcc.o: ..\Source\STM32F10xLib\M3\stm32f10x_conf.h
